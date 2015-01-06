3aa1bb5226173ae07221c00d6d323e0a3e9b60ea
Stefan Illgen <stefan.illgen@saxsys.de>
2015-01-06 15:54:45 +0100

    simple jersey service created; cleaned up superfluous contents

---
+++ Server/src/main/java/org/devel/jerseyfx/server/Starter.java
@@ -0,0 +1,48 @@
+package org.devel.jerseyfx.server;
+
+import java.net.URI;
+
+import javax.ws.rs.core.UriBuilder;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+import org.glassfish.grizzly.http.server.HttpServer;
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+public class Starter {
+
+	protected static final Logger logger = Logger.getLogger(Starter.class);
+
+	public static void main(String[] args) {
+		PropertyConfigurator.configure("log4j.properties");
+		runGrizzly();
+	}
+
+	private static void runGrizzly() {
+		URI baseUri = UriBuilder.fromUri("http://localhost/").port(9000)
+				.build();
+		ResourceConfig config = new ResourceConfig(MyResource.class);
+		HttpServer server = GrizzlyHttpServerFactory.createHttpServer(baseUri, config, false);
+
+		// register shutdown hook
+	    Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+	        @Override
+	        public void run() {
+	            logger.info("Stopping server..");
+	            server.shutdownNow();
+	        }
+	    }, "shutdownHook"));
+
+	    // run
+	    try {
+	        server.start();
+	        logger.info("Press CTRL^C to exit..");
+	        Thread.currentThread().join();
+	    } catch (Exception e) {
+	        logger.error(
+	                "There was an error while starting Grizzly HTTP server.", e);
+	    }
+	}
+
+}
