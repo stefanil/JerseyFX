2f5b44eac257caa155a686cb2bb73bee495c702e
Stefan Illgen <stefan.illgen@saxsys.de>
2015-01-06 16:24:58 +0100

    simple client using jersey client api

--- Client/src/main/java/org/devel/springfx/client/Starter.java
+++ Client/src/main/java/org/devel/springfx/client/Starter.java
@@ -1,9 +1,23 @@
 package org.devel.springfx.client;
 
+import javax.ws.rs.client.ClientBuilder;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
+
 public class Starter /* extends Application */{
 
+	protected static final Logger logger = Logger.getLogger(Starter.class);
+
 	public static void main(String[] args) {
-		
+		PropertyConfigurator.configure("log4j.properties");
+		runClient();
+	}
+
+	private static void runClient() {
+		logger.info(ClientBuilder.newClient().target("http://localhost:9000/")
+				.path("myresource")
+				.request().get(String.class));
 	}
 
 }
